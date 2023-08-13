2176aa4057de3a658ab3a7c6a264eb239ed5acaa
shahid-shafi <shahidshafi.org@gmail.com>
2023-08-13 23:10:53 +0530

    feat: create nodejs server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello from server 👋</h1>");
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("listening on port: ", port));
