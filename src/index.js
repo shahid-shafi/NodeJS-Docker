e6852bcffc32df9068bfb3e4987b328288adce07
shahid-shafi <shahidshafi.org@gmail.com>
2023-08-14 22:22:21 +0530

    adding: different files for different environments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello from server 👋</h1>");
+    res.send("<h1>Hello from server 👋!!!</h1>");
 })
 
 const port = process.env.PORT || 3000;
